e38b4442dd310400610e29f4ab7fdfd241264f8c
Iryna <iryna.sherepot@gmail.com>
2019-04-27 09:56:42 -0700

    start of tab component - box with vertical text grades

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import LeftTab from './components/LeftTab';
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Gradebook</h1>
       
-    </div>
+        <LeftTab></LeftTab>
+      </div>
+   // </div>
   );
 }
 
---
+++ src/components/LeftTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function LeftTab() {
+  return (
+    <div className = "LeftTab">
+      <h3>Grades</h3>
+      
+    </div>
+  );
+}
+
+export default LeftTab;
