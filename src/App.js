fd3c3dcde5671815e2ad0241444f00ef2222058e
Iryna <iryna.sherepot@gmail.com>
2019-04-27 12:18:34 -0700

    Ourside component created to hold tabs - 4 tabs showing horisontally - need to change to vertical layout and replace text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import LeftTab from './components/LeftTab';
+import LeftTabsColumn from './components/LeftTabsColumn';
 
 function App() {
   return (
     <div className="App">
       
       <h1>Gradebook</h1>
-      
+      <LeftTabsColumn></LeftTabsColumn>
         <LeftTab></LeftTab>
       </div>
    // </div>
---
+++ src/components/LeftTabsColumn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LeftTab from './LeftTab';
+import '../App.css';
+
+
+function LeftTabsColumn() {
+  return (
+    <div>
+       <LeftTab>
+      </LeftTab>
+      <LeftTab>
+      </LeftTab>
+      <LeftTab>
+      </LeftTab>
+    </div>
+  );
+}
+
+export default LeftTabsColumn;
