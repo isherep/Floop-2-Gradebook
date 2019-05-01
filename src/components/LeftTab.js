323dfa240d9bdc9b9d73c6109bfdf3e7fa84867d
Maria Kalusz <mkalusz1@gmail.com>
2019-04-30 21:58:17 -0700

    removed css import, only needed in app.js file

--- src/components/LeftTab.js
+++ src/components/LeftTab.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../App.css';
+
 
 class LeftTab extends Component{
   state = {
--- src/components/LeftTabsColumn.js
+++ src/components/LeftTabsColumn.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import LeftTab from './LeftTab';
-import '../App.css';
 
 class LeftTabsColumn extends Component {
 
