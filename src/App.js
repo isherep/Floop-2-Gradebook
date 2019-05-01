0c19b23bc5e05681aa3dee8f7a5a7d007495c8ac
Maria Kalusz <mkalusz1@gmail.com>
2019-04-30 21:52:37 -0700

    added grid component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
-
 import LeftTabsColumn from './components/LeftTabsColumn';
+import Grid from './components/grid';
 
 function App() {
   return (
     <div className="App">
-      
       <h1>Gradebook</h1>
-      
       <LeftTabsColumn /> 
-      
+      <Grid/>
       </div>
-   // </div>
   );
 }
 
