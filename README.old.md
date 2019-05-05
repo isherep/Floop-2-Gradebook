ef227bce3476f08dbc77da5fc186637d83407887
Maria Kalusz <mkalusz1@gmail.com>
2019-05-04 23:15:12 -0700

    grid done

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Floop-2-Gradebook
-Floop 2 team project repository for the Gradebook project
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,43 +1,16 @@
 import React, {Component} from 'react';
-
+import students from '../mock-data/students.json';
+import GridDetail from './gridDetail';
 
 class Grid extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            students: [
-                {
-                    id:1,
-                    name:'Bob',
-                    completed: false
-                },
-                {
-                    id:2,
-                    name:'Maria',
-                    completed: false 
-                },
-                {
-                    id:3,
-                    name:'Kevin',
-                    completed: false 
-                },
-                {
-                    id:4,
-                    name: 'Jose',
-                    completed: false
-                },
-            ]
-        };
-      }
-
     render() {
         return (
-            <div className = 'Grid'>
-                {this.state.students[0].name}
+            <div>
+                {students.map((studentItem, index)=>{
+                    return <GridDetail grid={studentItem}/>
+                })}
             </div>
         )
-      }
+            }
 }
-    
 export default Grid;
