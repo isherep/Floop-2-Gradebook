bcbcdb1584e40a58cd18d664781a48e3120762bc
Iryna <iryna.sherepot@gmail.com>
2019-05-14 12:58:17 -0700

    cleaned up comments

--- src/Data/database.js
+++ src/Data/database.js
@@ -17,21 +17,8 @@ const config = {
 
   let db = firebase.firestore();
 
-  // It's best to avoid declaring variables in the global space whenever possible. This can lead to hair pulling when you
-  // are trying to debug something and eventually realize that you've accidentally re-assigned a global variable inside of a
-  // function block or some other block of code. Break things into functions that return the the value you are trying to get
-  // whenever possible.
-
-  // If we were to export our students array up here like this:
-  //  
-  // //  export const students = []  
-  //
-  // it would be the same as the export default students statement at the bottom of this file. This will resolve before any of
-  // your other asynchronous code finishes running! Therefore exporting an empty array.
 const students = [];
-
   function getNumStudents() {
-
     return students.length
   }
 
@@ -39,21 +26,6 @@ const students = [];
 /*
   This entire function will run asynchronously and return a Promise that either resolves the value you intended to return
   or it will reject the promise if the condition fails and throw an error.
-
-  The Firestore query library uses an asynchronous promise based API. This essentially means that we are telling the javascript
-  runtime, "Hey, we want to fetch this data from somewhere, but we know it is going to take a while for the outside servers
-  to respond to our request. So go ahead and fetch this data, but don't stop any other code in our program from running while you
-  are out there fetching the data."
-
-  Because we are calling the Firestore .get() function on our firestore query, which already returns a promise,
-  We have to be careful how we structure the rest of our code in the getStudentsAsynchronouslyWithPromise function.
-  When we mix asynchronous code with synchronous calls, inside of a synchronous function, we are almost always
-  going to be bummed with the results. See the comments I've put above your original function 
-  for an example of this. Those synchronous calls are going to resolve before our Promises/asynchronous calls
-  resolved, often returning empty values, not building the objects we expect, and returning values in the wrong order.
-
-  Take a look at the comments through this function that explain it step by step...
-
 */
 const getStudentsAsynchronouslyWithPromise = function() {
   // Here we are already telling the runtime that this function will return a Promise. A Promise either
@@ -107,18 +79,7 @@ getStudentsAsynchronouslyWithPromise().then(response => console.log('With Promis
 
 /*
   This runs in exactly the same manner as the above function that returns the Promise, except in this function, we are using
-  ES2017 Async/Await syntax. It is basically what is called syntactic sugar on top of the Promise API. The intent here
-  is to make things a lot more readable and easier to follow. It also removes the potential for chaining promise 
-  after promise after promise in our code.
-
-  In this one, I stuck to es6 arrow function syntax to show you the difference in readability. There are some
-  differences in how the javascript runtime interprets/compiles function expressions and arrow functions, but let's not
-  worry too much about that right now.
-
-  When we use the keyword async in front of the function declaration, we are telling the runtime "Hey, this function
-  is going to return a promise, but we don't want to type out all that extra code, because it can get messy"
-
-  Let's move on and I will comment step by step how this code runs inside the function...
+  ES2017 Async/Await syntax. 
 */
 const getStudentsAsynchronously = async () => {
   //same as the other function. Build the query, but this time, we are going to store it inside
@@ -126,12 +87,6 @@ const getStudentsAsynchronously = async () => {
   const studentQuery = db.collection('Databases').doc('Dev_Database')
     .collection('Users').where('Role', '==', 'Student');
 
-  // This is the second part to the Async/Await syntax. We know that the firestore .get() function is going to
-  // run asynchronously and return a Promise that either resolves or rejects. But instead of using a .then() chain
-  // on the end of .get() we are going to use the await keyword instead. This basically says "Hey, we know this firestore
-  // method is going to return a promise so lets wait to run the rest of the code in this function 
-  // until it completely resolves. When it's done, we can store the value in the variable on the left hand of the
-  // assignment and then move on.
   const studentQuerySnapshot = await studentQuery.get();
 
   // declare the array that will store our custom objects
@@ -161,20 +116,7 @@ getStudentsAsynchronously().then(response => console.log('With Async/Await: ', r
 
 
 /*
-  This is your original function. This is a good example of how to give yourself a headache. I'm not dogging on you! This
-  Stuff is really hard to wrap your head around!
-  In a lot of the programming languages we have learned so far, we generally expect our code to run top down in the sequence
-  that it was written. But not JavaScript. It needs to be fast and able to handle many tasks at once. That is the whole
-  Idea between the JavaScript Asynchronous, Non-Blocking, runtime that Node.js and our browsers share alike. We don't want
-  our browser to freeze up everytime a small piece of javascript is running! 
-
-  Getting down to the nitty gritty, in this function we are mixing asynchronous calls with a synchronous function declaration.
-  And don't forget that the students array is declared outside of this function block! Even if we were to declare:
-
-  students = [];
-
-  inside of this function block, the return statement would still run before the Firestore .get() function resolves it's promise,
-  effectively returning an empty array again!
+  This is your original function. 
 
 */
 function getStudents() {
